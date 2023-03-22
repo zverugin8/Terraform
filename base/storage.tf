4c230a0d94e0b76ced8d35affee8493a39f10742
Siarhei Saroka <siarhei_saroka@epam.com>
2023-03-22 21:53:10 +0300

    task3 done

---
+++ base/storage.tf
@@ -0,0 +1,13 @@
+resource "random_string" "rnd" {
+  length  = 6
+  special = false
+}
+
+resource "google_storage_bucket" "task3_bucket" {
+  name                          = "epam-gcp-tf-lab-${random_string.rnd.result}"
+  location                      = "us-central1"
+  storage_class                 = "STANDARD"
+  force_destroy                 = true
+  uniform_bucket_level_access   = true
+  public_access_prevention      = "enforced"
+}
--- base/variables.tf
+++ base/variables.tf
@@ -1,4 +1,10 @@
 variable "ssh_key" {
-#  default     = null
+  #  default     = null
   description = "Provides custom public ssh key"
 }
+
+variable "project_id" {
+  default     = "saroka-gc-bootcamp"
+  description = "My project ID"
+}
+
