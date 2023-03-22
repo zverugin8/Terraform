88f5b91dbb70164b1ae8e974f0865b0c03942376
Siarhei Saroka <siarhei_saroka@epam.com>
2023-03-22 22:49:40 +0300

    task 3 fix1

--- base/storage.tf
+++ base/storage.tf
@@ -10,4 +10,9 @@ resource "google_storage_bucket" "task3_bucket" {
   force_destroy                 = true
   uniform_bucket_level_access   = true
   public_access_prevention      = "enforced"
+   labels                       = {
+    terraform = true
+    epam-tf-lab-owner = "${local.studentname}-${local.studentsurname}"
+
+  }
 }
